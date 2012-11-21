f23cfa510c8c57f2e22fc40ca7db5c57bd2345da
Xavier NOELLE <xavier.noelle@gmail.com>
2012-11-22 00:39:24 +0100

    Fix the hello spawner class example

--- examples/hello-spawner-class.rb
+++ examples/hello-spawner-class.rb
@@ -1,6 +1,10 @@
+$LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib")
+
 require 'spawner'
-s = Spawner::Conductor.new('/path/to/config.yml')
-1.upto(10) do |i|
-  s.add_duty(Proc.new() { puts "Task #{i}" })
+
+s = Spawner::Conductor.new("#{File.dirname(__FILE__)}/../etc/config.yml")
+1.upto(20) do |i|
+  s.add_duty(Proc.new() { puts "Task #{i}" }, 0)
 end
-s.join()
+
+s.join() {|ass, unass| puts "Still #{ass} jobs running, #{unass} jobs in the queue"}
