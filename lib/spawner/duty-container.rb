548e45010a2a6174fc3911d73e7068bf812576d6
Xavier NOELLE <xavier.noelle@gmail.com>
2012-11-25 19:44:31 +0100

    Lock a mutex before accessing the duty attribute

--- lib/spawner/duty-container.rb
+++ lib/spawner/duty-container.rb
@@ -5,21 +5,29 @@ module Spawner
   # handle workers persistency. Otherwise, we would not be able to hotswap a
   # DRb-exposed duty with another.
   class DutyContainer
+    attr_reader :duty
+
     def initialize()
       @duty = nil
       @duty_mutex = Mutex.new()
+    end
+
+    # Get the duty and discard it
+    def get_duty()
+      duty = nil
+
+      @duty_mutex.synchronize() do
+        duty = @duty
+        @duty = nil
+      end
+
+      return duty
     end
 
     def duty=(duty)
       @duty_mutex.synchronize() do
         @duty = duty
       end
-    end
-
-    def get_duty()
-      duty = @duty
-      @duty = nil
-      return duty
     end
   end
 end
