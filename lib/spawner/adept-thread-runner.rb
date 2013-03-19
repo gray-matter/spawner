f45cb4ae6f600860d8afec1255c30503be54b9c3
Xavier NOELLE <xavier.noelle@gmail.com>
2013-03-20 00:31:42 +0100

    [LIB/RUNNER] Very first version of capturing the out and err of a thread runner to a logger

--- lib/spawner/adept-thread-runner.rb
+++ lib/spawner/adept-thread-runner.rb
@@ -1,8 +1,24 @@
 require 'adept-runner'
 require 'thread'
+require 'string-io-redirector'
 
 module Spawner
   class AdeptThreadRunner < AdeptRunner
+    private
+    # The sole purpose of this class is to allow to redirect writes to
+    # std{out,err} to a method
+    class StringIORedirector < StringIO
+      def initialize(logger_method)
+        @logger_method = logger_method
+        super()
+      end
+
+      # FIXME: when writing without a line return, this will screw the log...
+      def write(*args)
+        @logger_method.call(*args)
+      end
+    end
+
     public
     def initialize()
       super()
@@ -14,20 +30,31 @@ module Spawner
     def start(persistent_worker)
       @adept_thread = Thread.new() do
         Thread.abort_on_exception = true
-        @job_mutex.synchronize() do
-          begin
-            duty = @duty_container.get_duty()
-
-            raise "Popping for #{object_id} gives nil" if duty.nil?()
-
+        begin
+          @job_mutex.synchronize() do
             begin
-              @adept.give_duty(duty)
-            rescue Exception => e
-              duty.report_failure(e)
-            end
+              duty = @duty_container.get_duty()
+              previous_stderr = nil
+              previous_stdout = nil
 
-            @no_more_duty_cond.wait(@job_mutex) if persistent_worker
-          end while persistent_worker
+              raise "Getting the duty for #{object_id} returns nil, please report this" if duty.nil?()
+
+              begin
+                previous_stdout, $stdout = $stdout, StringIORedirector.new(Spawner.jobs_logger.method(:debug))
+                previous_stderr, $stderr = $stderr, StringIORedirector.new(Spawner.jobs_logger.method(:error))
+                @adept.give_duty(duty)
+              rescue Exception => e
+                duty.report_failure(e)
+              ensure
+                $stdout = previous_stdout unless previous_stdout.nil?()
+                $stderr = previous_stderr unless previous_stderr.nil?()
+              end
+
+              @no_more_duty_cond.wait(@job_mutex) if persistent_worker
+            end while persistent_worker
+          end
+        rescue Exception => e
+          Spawner.spawner_logger.error("Exception raised in the thread runner: #{e}\n")
         end
       end
     end
